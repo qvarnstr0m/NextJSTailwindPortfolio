6417cfc49d155387dbcf162661e2403040240869
qvarnstr0m <mqvarnstrom80@gmail.com>
2023-03-03 10:40:52 +0100

    Testing paths for deployment

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,9 @@
 import Head from 'next/head';
-import NavBar from '../../components/NavBar';
+import NavBar from './components/NavBar';
 import Me from '../../components/Me';
 import Footer from '../../components/Footer';
 import Github from '../../components/Github';
-import Projects from 'components/Projects';
+import Projects from '../../components/Projects';
 import { ApolloClient, InMemoryCache, gql, createHttpLink } from '@apollo/client';
 import { setContext } from '@apollo/client/link/context';
 
