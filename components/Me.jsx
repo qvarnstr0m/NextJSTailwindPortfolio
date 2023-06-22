d62ed1c99b5aea54d21c583cd2595cd08b025cfa
qvarnstr0m <mqvarnstrom80@gmail.com>
2023-06-22 14:56:58 +0200

    Content changes on project

--- components/Me.jsx
+++ components/Me.jsx
@@ -49,9 +49,8 @@ const Me = () => {
           </a>{" "}
           and is a fullstack web app / SMS service built with React, .NET Web
           Api, MySql and a webscraper written in C#. If you are interested in
-          the code, version control or my planning, get in touch! Everything is
-          available on GitHub but as private repositories. There is also a scrum
-          board on Trello where I have organized my workflow. 💥
+          the code, version control or my planning, check out my github or get 
+          in touch! 💥
         </p>
       </div>
     </div>
