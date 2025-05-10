9fec75b26fb0853e97c35e0c434592b59288bd80
qvarnstr0m <mqvarnstrom80@gmail.com>
2025-05-10 17:26:38 +0200

    Content updates

--- components/Me.jsx
+++ components/Me.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Me = () => {
   return (
     <div className="md:flex">
-      <div className="sm:flex-col shrink-0 sm:ml-auto sm:order-1 md:pl-10">
+      <div className="sm:flex-col shrink-0 sm:ml-auto sm:order-1">
         <img
           src="me.jpg"
           alt="Profile image"
@@ -12,56 +12,47 @@ const Me = () => {
       </div>
       <div className="lg:max-w-xl mt-12 md:mt-0">
         <p className="text-start">
-          A problem-solving coder with a passion for learning new technologies.
-          Over the past few years, I have been focused on mastering the .NET
-          stack, primarily C# and web development with Blazor. Along the way,
-          I’ve also gained experience with React, ABP Framework, databases, EF
-          Core, Azure, and some CI/CD, either through work or personal projects.
-        </p>
-
-        <p className="text-start mt-6">
-          In June of 2024 I finished my 2 years at{" "}
-          <a
-            href="https://campus.varberg.se/yrkeshogskola/systemutvecklare-net"
-            target="_blank"
-            rel="noopener noreferer"
-          >
-            Campus Varberg
-          </a>{" "}
-          System Developer program, today I am currently working at{" "}
+          I work as a consultant at{" "}
           <a
             href="https://www.effectsoft.se"
-            target="_blank"
+            target="_self"
             rel="noopener noreferer"
           >
             Effectsoft
-          </a>{" "}
-          in Halmstad as a Software Consultant, where I did my internship,
-          mainly with .NET web applications, both front and backend, with
-          projects centered around multitenancy, system integrations, and
-          business platforms.
+          </a>
+          , Halmstad, Sweden. I have a diverse professional background with
+          experience from the transportation and hospitality sectors. After
+          adopting internet early I started building websites around the turn of
+          the millennium, lost interest, and later found my way back to
+          programming around 2020. In June Of 2024 I finished my two years at{" "}
+          <a
+            href="https://campus.varberg.se/yrkeshogskola/systemutvecklare-net"
+            target="_self"
+            rel="noopener noreferer"
+          >
+            Campus Varberg
+          </a>
+          .
+        </p>
+
+        <p className="text-start mt-6">
+          I mainly work with fullstack web-development using tools from the .NET
+          platform, but I aspire to be agnostic about the available tools and
+          platforms. I try to play around with tools and languages with personal
+          projects whenever possible.
+        </p>
+        <p className="text-start mt-6">
+          However, there are a few principles that I try to stick to: writing
+          readable and testable code, delivering value to users, applying clean
+          code practices, continuous learning and just having an open mindset in
+          general.
         </p>
 
         <p className="text-start mt-6">
           In my spare time I run a few times a week, climb and spend time with
           my two daughters, preferably outdoors. When I get the time I try to
-          broaden my horizon as a developer with new projects, going to meetups
-          and just trying new tech.
-        </p>
-
-        <p className="text-start mt-6">
-          🚀 My latest side project is live at{" "}
-          <a
-            href="https://www.insynskollen.se"
-            target="_blank"
-            rel="noopener noreferer"
-          >
-            insynskollen.se
-          </a>{" "}
-          and is a fullstack web app / SMS service built with React, .NET Web
-          Api, MySql and a webscraper written in C#. If you are interested in
-          the code, version control or my planning, check out my github or get
-          in touch! 🚀
+          broaden my horizon as a developer with new projects and going to
+          meetups.
         </p>
       </div>
     </div>
--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -5,7 +5,7 @@ const Navbar = () => {
     <div>
       <h1 className="text-6xl">Hello 👋🏼</h1>
       <h2 className="text-3xl">I am Martin Qvarnström,</h2>
-      <h3 className="text-2xl">a Software Developer</h3>
+      <h3 className="text-2xl">a software developer</h3>
     </div>
   );
 };
